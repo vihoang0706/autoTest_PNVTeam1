c74771d2d7aabfcfa74e58b28c68a95583183d53
TaoNguyen <tao.nguyen@student.passerellesnumeriques.org>
2019-04-16 11:39:48 +0700

    add new post

--- tests/adminNewPostTest.js
+++ tests/adminNewPostTest.js
@@ -1,7 +1,12 @@
 const url = require('../pages/utils/setUp');
+const randomNumber = Math.floor(Math.random()*10);
+const number = randomNumber;
+const titleName = 'Post: '+number+' Where were you';
+const content = 'When you were in trouble and you needed a hand I was always there. When your legs got shaky and you could not stand I was always there. I was always there when you needed me the most. When trouble comes a-knocking and I needed a hand. Where will you be then? When you needed a hand, I am always ready and when I tired you, you where were you place?';
+
 module.exports = {
     tags :['addPostFunction'],
-
+    
     before: function (browser) {
         url.openBrowser(browser);
         const login = browser.page.adminLoginPage();
@@ -9,7 +14,26 @@ module.exports = {
             .fillInLoginForm('TaoNguyen','thientrieuquoc01247')
             .assert.title('Dashboard ‹ Auto — WordPress')
     },
-
-    
+    'Navigate link post': function (browser)  {
+        const post = browser.page.adminPostPage();
+        post 
+            .navigateMenuItem()
+    },
+    'Click sub menu item in post': function (browser) {
+        const post = browser.page.adminPostPage();
+        post
+            .goToAddNewPost()
+            .cancleTip()
+    },
+    'Post information': function (browser) {
+        const post = browser.page.adminPostPage();
+        post    
+            .fillInData(titleName, content)
+            .saveInfor()
+            .clickViewPost()
+            .assert.containsText('@h1TiltePostCheck',titleName)
+            .assert.containsText('@paragraphContentCheck',content)
+    },
+   
 };
 

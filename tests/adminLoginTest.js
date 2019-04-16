e16b890965b2f5af5ad8558be3cba633ad3cdcf4
InternDN19.01.04 Hoang Thi Thao Vi <InternDN19.01.04@logigear.com>
2019-04-16 14:35:22 +0700

    Run test cases and remove draft files

--- tests/adminLoginTest.js
+++ tests/adminLoginTest.js
@@ -1,13 +1,26 @@
 const username = 'admin';
 const password = '123456789';
 const url = require('../pages/utils/setUp');
+const assertAccountUser = '//li[@class="menupop with-avatar"]';
 
 module.exports = {
-    '@tags':['login1'],
-    'Login with valid information': function(browser) {
+    '@tags': ['logintest'],
+    before: function (browser) {
         url.openBrowser(browser);
+    },
+    'Login with valid information': function (browser) {
         var login = browser.page.adminLoginPage();
-        login.fillInLoginForm(username,password);
-        browser.end();
+        login.fillInLoginForm(username, password);
+        browser.useXpath()
+            .pause(1000);
+        browser.assert.visible(assertAccountUser);
+        browser.execute(function () {
+            document.querySelector('#wp-admin-bar-logout > a').click();
+        });
+    },
+    after: function (browser) {
+        browser
+            .pause(2000)
+            .end();
     }
 };

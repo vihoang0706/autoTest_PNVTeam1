626ce9b9a0ecb4a904d7fa959ad9b402f1a90d71
InternDN19.01.04 Hoang Thi Thao Vi <InternDN19.01.04@logigear.com>
2019-04-16 13:24:15 +0700

    Add some new functions: quick edit, delete tags

--- pages/adminTagPage.js
+++ pages/adminTagPage.js
@@ -1,7 +1,7 @@
 module.exports = {
     url: 'http://localhost/team1_theme2/wordpress/wp-login.php',
     elements: {
-        // Post
+        //Link go to Tag
         linkPosts: {
             selector: '//div[@class="wp-menu-name" and text()= "Posts"]',
             locateStrategy: 'xpath'
@@ -10,6 +10,8 @@ module.exports = {
             selector: '//li[@id="menu-posts"]//a[text()="Tags"]',
             locateStrategy: 'xpath'
         },
+
+        // Add Tag Form
         inputTagName: {
             selector: '#tag-name',
             locateStrategy: 'css selector'
@@ -20,9 +22,11 @@ module.exports = {
         },
         inputDescription: '#tag-description',
         buttonAddNewTag: '#submit',
+
+        // Edit Form
         inputEditName: '#name',
         inputEditSlug: '#slug',
-        inputEditDescription:'#description',
+        inputEditDescription: '#description',
         buttonUpdate: {
             selector: '//div[@class="edit-tag-actions"]/input',
             locateStrategy: 'xpath'
@@ -31,22 +35,38 @@ module.exports = {
             selector: '//div[@id="message"]/p/a[contains(text()," Back to Tags")]',
             locateStrategy: 'xpath'
         },
-        linkDeleteTag: {
-            selector: 'div.row-actions > span.delete > a',
-            locateStrategy: 'css selector'
+
+        // Delete All Tags
+        checkboxSelectAll: '#cb-select-all-2',
+        buttonDeleteBulkAction: '#bulk-action-selector-bottom > option:nth-child(2)',
+        buttonApply: '#doaction2',
+
+        // Quick Edit Form
+        inputTitleTextWrap: {
+            selector: '//tr[@class="inline-edit-row inline-editor"]/td/fieldset/div/label[1]/span[2]/input',
+            locateStrategy: 'xpath'
+        },
+        inputSlugTextWrap: {
+            selector: '//tr[@class="inline-edit-row inline-editor"]/td/fieldset/div/label[2]/span[2]/input',
+            locateStrategy: 'xpath'
+        },
+        buttonUpdateTagWrap: {
+            selector: '//tr[@class="inline-edit-row inline-editor"]/td/div/button[@class="save button button-primary alignright"]',
+            locateStrategy: 'xpath'
+        },
+        // Check Column Actual
+        columnActualTitle: {
+            selector: '(//table[@class="wp-list-table widefat fixed striped tags"]//tbody/tr//td[@class="name column-name has-row-actions column-primary"]/strong/a)[1]',
+            locateStrategy: 'xpath'
+        },
+        columnActualDescription: {
+            selector: '(//table[@class="wp-list-table widefat fixed striped tags"]//tbody/tr/td[@class="description column-description"]/p)[1]',
+            locateStrategy: 'xpath'
+        },
+        columnActualSlug: {
+            selector: '(//table[@class="wp-list-table widefat fixed striped tags"]//tbody/tr//td[@class="slug column-slug"])[1]',
+            locateStrategy: 'xpath'
         }
-        // rowTitle: {
-        //     selector:'(//table[@class="wp-list-table widefat fixed striped tags"]//tbody/tr//td[@class="name column-name has-row-actions column-primary"]/strong/a)[1]',
-        //     locateStrategy: 'xpath'
-        // },
-        // rowDescription: {
-        //     selector: '(//table[@class="wp-list-table widefat fixed striped tags"]//tbody/tr//td[@class="description column-description"]/p)[1]',
-        //     locateStrategy: 'xpath'
-        // },
-        // rowSlug: {
-        //     selector: '(//table[@class="wp-list-table widefat fixed striped tags"]//tbody/tr//td[@class="slug column-slug"])[1]',
-        //     locateStrategy: 'xpath'
-        // },
     },
     commands: [{
         gotoTagPage() {
@@ -54,44 +74,46 @@ module.exports = {
                 .click('@linkPosts')
                 .click('@linkTags');
         },
-        fillInTagsForm(tagsName,slugName,description){
+        fillInTagForm(tagsName, slugName, description) {
             return this
-                .setValue('@inputTagName',tagsName)
-                .setValue('@inputSlugName',slugName)
-                .setValue('@inputDescription',description)
+                .setValue('@inputTagName', tagsName)
+                .setValue('@inputSlugName', slugName)
+                .setValue('@inputDescription', description)
                 .click('@buttonAddNewTag');
         },
-        goBackToTag() {
+        goBackToTagPage() {
             return this
                 .click('@linkBackToTag');
         },
-        deleteTag() {
-            return this
-                .click('@linkDeleteTag');
-        },
-        setQuery(value) {
-            return this
-                .setValue('@mainQueryInput', value);
-        },
-        selectFilter(selector, value) {
-            return this
-                .click(selector)
-                .click(`.goog-menuitem[value="${value}"]`);
-        },
-        search() {
-            return this
-                .click('@submitButton');
-        },
-        editTagForm(tagsName,slugName,description){
+        editTagForm(tagsName, slugName, description) {
             return this
                 .clearValue('@inputEditName')
-                .setValue('@inputEditName',tagsName)
+                .setValue('@inputEditName', tagsName)
                 .clearValue('@inputEditSlug')
-                .setValue('@inputEditSlug',slugName)
+                .setValue('@inputEditSlug', slugName)
                 .clearValue('@inputEditDescription')
-                .setValue('@inputEditDescription',description)
+                .setValue('@inputEditDescription', description)
                 .click('@buttonUpdate');
-                
+
+        },
+        quickEditTagForm(tagsName, slugName,){
+            return this
+                .clearValue('@inputTitleTextWrap')
+                .setValue('@inputTitleTextWrap', tagsName)
+                .clearValue('@inputSlugTextWrap')
+                .setValue('@inputSlugTextWrap', slugName)
+                .click('@buttonUpdateTagWrap');
+        },
+        deleteAllTags() {
+            return this
+                .click('@checkboxSelectAll')
+                .click('@buttonDeleteBulkAction')
+                .click('@buttonApply');
+        },
+        deleteTag(){
+            return this
+                .pause(1000)
+                .acceptAlert();
         }
     }]
 };

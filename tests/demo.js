2abcf6df7c502d6c4a599389d9c5f53d1d2a38ff
InternDN19.01.04 Hoang Thi Thao Vi <InternDN19.01.04@logigear.com>
2019-04-16 13:55:09 +0700

    Solve conflicts

--- tests/demo.js
+++ tests/demo.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const login1 = require('../pages/utils/setUp');
 module.exports = {
     '@tags': ['login1'],
@@ -8,15 +7,3 @@ module.exports = {
         login1.selectMainMenu(browser,'//li/a[contains(text(),"Tiếng Anh")]');
     }
 }
-=======
-// tests.js
-var utils = require('../pages/utils/setUp.js')
-module.exports = {
-  '@tags': ['sampletest'],
-
-  'Open the website': function (browser) {
-    utils.openBrowser(browser);
-    // login(browser).goToSite();
-  },
-};
->>>>>>> 349c4495e12ee112c6d905f9e21f738448219ea8

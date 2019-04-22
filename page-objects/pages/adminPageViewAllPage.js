db40c982389f8b643cb05a733ffe18f6093057c9
InternDN19.01.01 Nguyen Thi Thuyen Quyen <InternDN19.01.01@logigear.com>
2019-04-22 17:14:14 +0700

    writing automation test scrip, login valid information, login with invalid information, logout, create new page

---
+++ page-objects/pages/adminPageViewAllPage.js
@@ -0,0 +1,43 @@
+module.exports = {
+    commands: [{
+        checkContainsText(element, expectedContain) {
+            return this.assert.containsText('@'+element, expectedContain)
+        },
+        deleteAllPages() {
+            return this
+                .click('@checkboxSelectAllPage')
+                .click('@selectOptionTrash')
+                .click('@buttonApply');
+        },
+        goToAction(element) {
+            return this
+                .waitForElementVisible('@columnActualTitle')
+                .moveToElement('@columnActualTitle', 0, 0)
+                .waitForElementVisible(element)
+                .click(element);
+        }
+    }],
+    elements: {
+        // Check Column Actual
+        columnActualTitle: {
+            selector: '(//table[@class="wp-list-table widefat fixed striped pages"]//tbody/tr//td[@class="title column-title has-row-actions column-primary page-title"]/strong/a)[1]',
+            locateStrategy: 'xpath'
+        },
+        linkTrashPage: {
+            selector: '//div[@class="row-actions"]//span[@class="trash"]/a[@class="submitdelete"]',
+            locateStrategy: 'xpath'
+        },
+        checkboxSelectAllPage: {
+            selector: '//input[@id="cb-select-all-2"]',
+            locateStrategy: 'xpath'
+        },
+        selectOptionTrash: {
+            selector: '//select[@id="bulk-action-selector-bottom"]/option[@value="trash"]',
+            locateStrategy: 'xpath'
+        },
+        buttonApply: {
+            selector: '//input[@id="doaction2"]',
+            locateStrategy: 'xpath'
+        }
+    }
+};

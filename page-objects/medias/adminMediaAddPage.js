453dfe4eccb78baab5d81cb72e93602d9713dead
TaoNguyen <tao.nguyen@student.passerellesnumeriques.org>
2019-04-22 17:03:08 +0700

    Add media

--- page-objects/medias/adminMediaAddPage.js
+++ page-objects/medias/adminMediaAddPage.js
@@ -1,10 +1,41 @@
+const goToPost = require("../base-objects/adminBasePage");
 module.exports = {
     commands: [{
+        addNewMedia(image) {
+            return this
+            .setValue('@inputImage', require('path').resolve(image))
+            .click('@buttonUpload')
+            .pause('1000')
+        },
+        checkCorrectImage(element, nameImage) {
+            return this.assert.containsText('@'+element, nameImage)
+        },
 
+        clickHideLine(element) {
+            return this
+            .moveToElement('@fristTableRow', 0, 0)
+            .click(element)
+        }
     }],
     elements: {
-        buttonSelectFiles: {
-            selector: '//input[@id="plupload-browse-button"]',
+        inputImage: {
+            selector: '//input[@id="async-upload"]',
+            locateStrategy: 'xpath'
+        },
+        buttonUpload: {
+            selector: '//input[@id="html-upload"]',
+            locateStrategy: 'xpath'
+        },
+        image: {
+            selector: '//tr[1]/td/strong[@class="has-media-icon"]/a',
+            locateStrategy: 'xpath'
+        },
+        fristTableRow: {
+            selector: '//tbody[@id="the-list"]/tr[1]/td[1]',
+            locateStrategy: 'xpath'
+        },
+        linkDeleteImage: {
+            selector: '//span[@class="edit"]/a[@class="submitdelete aria-button-if-js" and text() ="Delete Permanently"]',
             locateStrategy: 'xpath'
         }
     }
--- tests/medias/adminMediaAddTest.js
+++ tests/medias/adminMediaAddTest.js
@@ -1,25 +1,38 @@
 const utils = require('../../page-objects/utils/setUp');
-let addMedia;
- module.exports = {
-     tags: ['addMediaFunction'],
+let addMedia, dashboard, login, username, password;
+let imageName = 1234, extension = '.jpg';
+let image = 'C:/Users/InternDN19.01.05/'+imageName+extension;
+module.exports = {
+    tags: ['addMediaFunction'],
 
-     before: function (browser) {
-         login = browser.page.adminUserLoginPage();
-         dashboard = browser.page.adminBasePage();
-        //  addMedia = browser.page.adminMediaAddPage();
-         username = browser.globals.userNames.username;
-         password = browser.globals.userNames.password;
+    before: function (browser) {
+        login = browser.page.adminUserLoginPage();
+        dashboard = browser.page.adminBasePage();
+        addMedia = browser.page.adminMediaAddPage();
+        username = browser.globals.userNames.username;
+        password = browser.globals.userNames.password;
 
-         utils.openBrowser(browser);
-         login.login(username, password);
-     },
-
-    'Step 1: Go to media page ': function () {
-        dashboard.goToPage('@linkMedia', '@linkAddNewMedia');
+        utils.openBrowser(browser);
+        login.login(username, password);
     },
 
-    // 'Step 2: Add media' : function (){
+    'Step 1: Go to media page ': function () {
+        dashboard.goToPage('@linkMedia', '@linkAddNewMedia')
+    },
 
-    // }
+    'Step 2: Add media': function () {
+        addMedia
+            .addNewMedia(image)
+            .checkCorrectImage('image', imageName)
+    },
 
- }
+    after: function (browser) {
+        addMedia
+            .clickHideLine('@linkDeleteImage')
+        browser
+            .pause(1000)
+            .acceptAlert()
+            .end()
+    }
+
+}

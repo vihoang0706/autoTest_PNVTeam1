2c8900def0aa6b65e8cf8b2c498c9568a9e81df0
TaoNguyen <tao.nguyen@student.passerellesnumeriques.org>
2019-04-23 08:29:21 +0700

    Refactor AddPost

--- tests/posts/adminPostAddTest.js
+++ tests/posts/adminPostAddTest.js
@@ -1,20 +1,25 @@
-const utils = require('../../page-objects/utils/setUp');
 const titleName = 'Post: ' + (Math.floor(Math.random() * 50)) + ' Where were you';
 const content = 'When you were in trouble and you needed a hand I was always there. When your legs got shaky and you could not stand I was always there. I was always there when you needed me the most. When trouble comes a-knocking and I needed a hand. Where will you be then? When you needed a hand, I am always ready and when I tired you, you where were you place?';
 let dashboard, addPost, login, username, password;
 
 module.exports = {
     tags: ['addPostFunction'],
-    before: function (browser) {
-        login = browser.page.adminUserLoginPage();
-        dashboard = browser.page.adminBasePage();
-        addPost = browser.page.adminPostAddPage();
-        username = browser.globals.userNames.username;
-        password = browser.globals.userNames.password;
-        utils.openBrowser(browser);
+    // before: function (browser) {
+    //     login = browser.page.adminUserLoginPage();
+    //     dashboard = browser.page.adminBasePage();
+    //     addPost = browser.page.adminPostAddPage();
+    //     username = browser.globals.userNames.username;
+    //     password = browser.globals.userNames.password;
+    //     utils.openBrowser(browser);
+    //     login.login(username, password);
+    // },
+
+    'Pre-condition: Login with valid account': function (browser) {
+        const login = browser.page.adminUserLoginPage();
+        var username = browser.globals.userNames.username;
+        var password = browser.globals.userNames.password;
         login.login(username, password);
     },
-
     'Step 1: Go to post page ': function () {
         dashboard.goToPage('@linkPosts', '@linkNewPost');
     },
@@ -26,10 +31,10 @@ module.exports = {
             .checkContainsText('actualParagraphContent', content)
     },
     
-    after: function () {
-        addPost
-            .stop()
-            .pause(1000)
-            .end();
-    }
+    // after: function () {
+    //     addPost
+    //         .stop()
+    //         .pause(1000)
+    //         .end();
+    // }
 };

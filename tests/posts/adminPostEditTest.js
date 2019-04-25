03e5ec60be5c6966258194b3eaa482cb433a876b
TaoNguyen <tao.nguyen@student.passerellesnumeriques.org>
2019-04-25 14:48:16 +0700

    Conversion checkContentText to getContentValue function

--- tests/posts/adminPostEditTest.js
+++ tests/posts/adminPostEditTest.js
@@ -1,6 +1,7 @@
 const titleName = 'Post: ' + (Math.floor(Math.random() * 50)) + ' My friend';
 const content = 'When you were in trouble and you needed a hand I was always there. When your legs got shaky and you could not stand I was always there. I was always there when you needed me the most. When trouble comes a-knocking and I needed a hand. Where will you be then? When you needed a hand, I am always ready and when I tired you, you where were you place?';
 const updatecontent = 'I have some best friends their names are: Delia and Sofia. They are all really nice we all get along very well I only fought with Laura once and with Delia none! But I’ve fought with Sofia a few times (she is my sister). Anyway we get along very well and we have a lot of fun tougher, we all like music, playing and so on, we really enjoy spending time with each other.';
+const assert = require('assert');
 let dashboard, addPost, login, username, password, editPost;
 
 module.exports =  {
@@ -10,6 +11,7 @@ module.exports =  {
         addPost =  browser.page.adminPostAddPage();
         editPost = browser.page.adminPostEditPage();
         dashboard = browser.page.adminBasePage();
+
         username = browser.globals.userNames.username;
         password = browser.globals.userNames.password;
         login.login(username, password);
@@ -22,10 +24,10 @@ module.exports =  {
     'Step 2: Edit Information in post': function() {
         editPost.editPost('', updatecontent)
         dashboard.goToPage('linkPosts', 'linkAllPosts');
-        addPost
-            .checkContainsText('titleActual',titleName)
-            .clickPostItem();
-        editPost.checkContainsText('actualContentPost', updatecontent);
+
+        addPost.getContentValue('actualTitle', function(textFromPage){assert.equal(textFromPage, titleName)})
+        addPost.clickPostItem();
+        editPost.getContentValue('actualContent', function(textFromPage){assert.equal(textFromPage, updatecontent)})
     },
     'Pre-condition: Delete Post': function () {
         dashboard.goToPage('linkPosts', 'linkAllPosts');

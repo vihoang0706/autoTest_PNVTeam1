397fb1f89dd2339e09d9dda24e38a2cd787e6053
InternDN19.01.04 Hoang Thi Thao Vi <InternDN19.01.04@logigear.com>
2019-05-14 09:12:46 +0700

    Delete file formatElements and drivers follders

--- commons/custom-commands/formatElements.js
+++
@@ -1,5 +0,0 @@
-const util = require("util");
-exports.command = function formatElement(elementName, data) {  
-    var element = this.elements[elementName.slice(1)];
-    return util.format(element.selector, data);
-  };

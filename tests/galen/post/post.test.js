8eaeee3bb2a65224bfdaba2d793d5c3f06b801ea
InternDN19.01.04 Hoang Thi Thao Vi <InternDN19.01.04@logigear.com>
2019-05-16 09:16:55 +0700

    Edit file post.test.js

--- tests/galen/post/post.test.js
+++ tests/galen/post/post.test.js
@@ -2,13 +2,17 @@ load("../init.js");
 load("../../../page-objects/galen/login/loginPage.js");
 load("../../../page-objects/galen/post/postPage.js");
 load("../../../page-objects/galen/dashboard/dashboard.js");
-testOnAllDevices("Verify that post page displays correctly ", "/wp-login.php", function (driver, device) {
+testOnDevice("desktop","Post Page", "/wp-login.php", function (driver, device) {
     var loginPage = null;
-    loginPage = new LoginPage(driver).waitForIt();
-    loginPage.loginAs('admin', '123456789');
-    dashboardPage = new DashboardPage(driver).waitForIt();
-    dashboardPage.goToPage('Add Post');
-    postPage = new PostPage(driver).waitForIt();
-    postPage.dissmissTip();
-    checkLayout(driver, "specs/post/postPage.gspec", device.tags);
+    var dashboardPage = null;
+    var postPage = null;
+    logged("Verify that post page displays all elements correctly ", function () {
+        loginPage = new LoginPage(driver).waitForIt();
+        loginPage.loginAs(TEST_USER.username,TEST_USER.password);
+        dashboardPage = new DashboardPage(driver).waitForIt();
+        dashboardPage.goToPage('Add Post');
+        postPage = new PostPage(driver).waitForIt();
+        postPage.dissmissTip();
+        checkLayout(driver, "specs/post/postPage.gspec", device.tags);
+    });
 });

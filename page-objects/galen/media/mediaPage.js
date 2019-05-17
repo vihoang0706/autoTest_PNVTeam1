a7a6cbab632ec01d09a8069f817bb5764f4a418f
TaoNguyen <tao.nguyen@student.passerellesnumeriques.org>
2019-05-17 09:28:29 +0700

    edit media

--- page-objects/galen/media/mediaPage.js
+++ page-objects/galen/media/mediaPage.js
@@ -1,4 +1,9 @@
 this.MediaPage = $page("Add media",{
     inputImage: "input#async-upload",
-    inputUpload: "input#html-upload"
+    ButtonUpload: "input#html-upload"
+}, {
+    addMediaFunction: function (imageName) {
+        this.inputImage.require('path').resolve(__dirname + imageName);
+        this.ButtonUpload.click();
+      }
 });
--- tests/galen/media/media.test.js
+++ tests/galen/media/media.test.js
@@ -2,6 +2,7 @@ load("../init.js");
 load("../../../page-objects/galen/login/loginPage.js");
 load("../../../page-objects/galen/dashboard/dashboardPage.js");
 load("../../../page-objects/galen/media/mediaPage.js");
+var image = '../../../images/girl.jpg';
 
 testOnDevice("desktop", "Media", "/wp-login.php", function (driver, device) {
     var loginPage = null; 
@@ -14,6 +15,7 @@ testOnDevice("desktop", "Media", "/wp-login.php", function (driver, device) {
         dashboardPage = new DashboardPage(driver).waitForIt();
         dashboardPage.goToPage("Add New Media");
         mediaPage = new MediaPage(driver).waitForIt();
+        mediaPage.addMediaFunction(image)
         checkLayout(driver, "specs/media/mediaPage.gspec", device.tags);
     });
 });

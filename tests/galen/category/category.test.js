286abe36b1750747a1c279547b98e0123d34344f
hiennguyenpnv98 <hien.nguyenpnv1998@gmail.com>
2019-05-21 10:20:08 +0700

    demo jenkins1

--- tests/galen/category/category.test.js
+++ tests/galen/category/category.test.js
@@ -4,7 +4,8 @@ load("../../../page-objects/galen/dashboard/dashboardPage.js");
 load("../../../page-objects/galen/category/categoryPage.js");
 
 testOnDevice("desktop", "Category", "/wp-login.php", function (driver, device) {
-    var loginPage  = null, dashboardPage = null;
+    var loginPage  = null;
+    var dashboardPage = null;
 
     logged("Verify that the category page have enough elements", function () {
         loginPage = new LoginPage(driver).waitForIt();

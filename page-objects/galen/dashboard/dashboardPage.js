03e6823922c465b50f18c856ad95ce65eee2ab32
InternDN19.01.04 Hoang Thi Thao Vi <InternDN19.01.04@logigear.com>
2019-05-23 15:24:24 +0700

    Create page dump and generate spec

--- page-objects/galen/dashboard/dashboardPage.js
+++ page-objects/galen/dashboard/dashboardPage.js
@@ -1,6 +1,12 @@
 this.DashboardPage = $page("Dashboard Page", {
     linkPost: "//div[@class='wp-menu-name' and text()= 'Posts']",
     linkMedia: "//div[@class='wp-menu-name' and text()= 'Media']",
+    navigation: "//div[@id='wpwrap']//div[@id='wpcontent']//li[@id='wp-admin-bar-menu-toggle']//span[@class='ab-icon']",
+    goToNavigation:function() {
+        if (this.navigation.isDisplayed()) {
+            this.navigation.click(); 
+        } 
+    },
     goToPage: function (namePage) {
         var linkCategory = "//li[@id='menu-posts']//a[text()='Categories']";
         var linkNewPost = "//li[@id='menu-posts']//a[text()='Add New']";
--- tests/galen/post/post.test.js
+++ tests/galen/post/post.test.js
@@ -3,7 +3,7 @@ load("../../../page-objects/galen/login/loginPage.js");
 load("../../../page-objects/galen/post/postPage.js");
 load("../../../page-objects/galen/dashboard/dashboardPage.js");
 
-testOnDevice("mobile","Verify that post page displays correctly ", "/wp-login.php", function (driver, device) {
+testOnAllDevices("Verify that post page displays correctly ", "/wp-login.php", function (driver, device) {
     var loginPage = null;
     var dashboardPage = null;
     var postPage = null;
@@ -11,20 +11,11 @@ testOnDevice("mobile","Verify that post page displays correctly ", "/wp-login.ph
     logged("Verify that post page displays correctly ", function () {
         loginPage = new LoginPage(driver).waitForIt();
         loginPage.login('admin', '123456789');
-        dashboardPage = new DashboardPage(driver).waitForIt();
+        dashboardPage = new DashboardPage(driver);
+        dashboardPage.goToNavigation();
         dashboardPage.goToPage('Add New Post');
         postPage = new PostPage(driver).waitForIt();
         postPage.dissmissTip();
-        dumpPage({
-            driver: driver,
-            name: "Home page",
-            spec: "specs/post/postPage.gspec",
-            exportPath: "dumps/post",
-            maxWidth: 200,
-            maxHeight: 200,
-            onlyImages: false,
-            excludedObjects: ["header", "footer"]
-        });
-        // checkLayout(driver, "specs/post/postPage.gspec", device.tags);
+        checkLayout(driver, "specs/post/postPage.gspec", device.tags);
     });
 });

d1f1969c15e0ea67870b67bbb12f05789966f079
vihoang <vi.hoang@student.passerellesnumeriques.org>
2019-05-25 08:30:36 +0700

    Add report files

--- globals.js
+++ globals.js
@@ -3,28 +3,29 @@ var userNames = {
   username: 'admin',
   password: '123456789',
 };
-const HtmlReporter = require('@rpii/nightwatch-html-reporter');
+// const HtmlReporter = require('@rpii/nightwatch-html-reporter');
  
-const htmlReporter = new HtmlReporter({
-    openBrowser: true,
-    reportsDirectory: __dirname + '/reports',               
-    uniqueFilename: true,
-    customTheme: 'html-reporter.html',
-});
+// const htmlReporter = new HtmlReporter({
+//     openBrowser: true,
+//     reportsDirectory: __dirname + '/reports',               
+//     uniqueFilename: true,
+//     customTheme: 'html-reporter.html',
+// });
 module.exports = {
   userNames: userNames,
   paths: {
     data: path.resolve(__dirname, './data/data.csv')
   },
-  "reporter" : htmlReporter.fn,
+  // "reporter" : htmlReporter.fn,
   beforeEach: async function (browser, done) {
     browser
       .maximizeWindow()
-      .url('http://192.168.189.70/wordpress/wp-login.php');
+      // .url('http://192.168.189.70/wordpress/wp-login.php');
+      .url('http://localhost/team1_theme2/wordpress/wp-login.php');
     browser.perform(function () {
       done();
     });
-    htmlReporter.setBrowserOptions( browser.options);
+    // htmlReporter.setBrowserOptions( browser.options);
   },
   afterEach: function (browser,done) {
     browser.end(function () {
